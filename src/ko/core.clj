0e48f8ee2d869e9fc5640583eca5c07a74ed464a
bion <bion@citizencode.io>
2015-09-22 23:19:56 -0700

    Blind stab at play-score

--- src/ko/core.clj
+++ src/ko/core.clj
@@ -1,7 +1,39 @@
 (ns ko.core
+  [:use [overtone.core]]
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defsynth sin-synth [amp 1 freq 200 outbus 0]
+  (out outbus (* amp (sin-osc freq 0))))
+
+(def nome (metronome 120))
+
+(defn- quant-to-beat-in-bar [quant beat-zero]
+  (+ beat-zero (- quant 1)))
+
+(defn- play-event [event]
+  ((:instr event)
+   :freq (:freq event)
+   :amp (:amp event)
+   :env (:env event)))
+
+(defn- schedule-measure [measure next-bar-beat]
+  (doseq [[quant event] measure]
+    (let [beat (quant-to-beat-in-bar quant next-bar-beat)]
+      (at (nome beat) (play-event event)))))
+
+(defn- schedule-cycle [measures]
+  (let [next-measure (first measures)
+        remaining-measures (rest measures)
+        ;; measures are scheduled one beat before they begin
+        next-bar-beat (metro-bar nome)
+        next-cycle-timestamp (nome (+ 3 next-bar-beat))]
+
+    (schedule-measure next-measure next-bar-beat)
+
+    (apply-by next-cycle-timestamp
+              #'schedule-cycle
+              remaining-measures)))
+
+(defn play-score [score]
+  (nome :bpm (:bpm score))
+  (schedule-cycle (:measures score)))
