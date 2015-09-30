20297f2852aaa074484d4979cf81b4cc10f25530
bion <bion@citizencode.io>
2015-09-29 21:59:48 -0700

    Don't use metronome

--- src/ko/core.clj
+++ src/ko/core.clj
@@ -3,61 +3,78 @@
   (:gen-class))
 
 (defsynth sin-synth [amp 1 freq 440 outbus 0]
-  (out outbus (* amp
-                 (sin-osc freq)
-                 (env-gen:kr (envelope [0 1 0] [0.01 0.3] [-1 -1]) :action 2))))
+  (out outbus
+       (* amp
+          (sin-osc freq)
+          (env-gen:kr
+           (envelope [0 1 0]
+                     [0.01 0.3]
+                     [-1 -1])
+           :action 2))))
 
-(def nome (metronome 120))
+(def ^:dynamic beats-per-bar 4)
+(def ^:dynamic beats-per-minute 120)
 
-(defn- quant-to-beat-in-bar [quant beat-zero]
-  (+ beat-zero (- quant 1)))
+(defn calc-beat-dur []
+  (/ 60.0 beats-per-minute))
+
+(defn calc-beat-dur-ms []
+  (* (calc-beat-dur) 1000))
+
+(defn quant-to-timestamp
+  "Given a quant (e.g. the off-beat of one would be '1.5'),
+  beat duartion, and offset returns number timestamp in
+  milliseconds from the offset"
+  [quant offset beat-duration]
+    (+ offset (* (- quant 1) (calc-beat-dur-ms))))
 
 (defn- play-event [event]
   ((:instr event)
    :freq (:freq event)
    :amp (:amp event)))
 
-(defn- schedule-measure [measure next-bar-beat]
+(defn- schedule-measure [measure next-bar-timestamp]
   (doseq [[quant events] (partition 2 measure)]
-    (let [beat (quant-to-beat-in-bar quant next-bar-beat)]
-      (at (nome beat)
+    (let [timestamp (quant-to-timestamp quant next-bar-timestamp (calc-beat-dur))]
+      (at timestamp
           (doseq [event events]
             (play-event event))))))
 
-(defn- schedule-cycle [measures]
+;; measures are scheduled one beat before they begin
+(defn- schedule-cycle [measures current-time]
   (let [next-measure (first measures)
         remaining-measures (rest measures)
-        ;; measures are scheduled one beat before they begin
-        next-bar-beat (* 4 (metro-bar nome))
-        next-cycle-timestamp (nome (+ 3 next-bar-beat))]
+        beat-dur-ms (calc-beat-dur-ms)
+        next-bar-timestamp (+ current-time beat-dur-ms)
+        next-cycle-timestamp (+ current-time (* beat-dur-ms beats-per-bar))]
 
-    (schedule-measure next-measure next-bar-beat)
+    (schedule-measure next-measure next-bar-timestamp)
 
-    (apply-by next-cycle-timestamp
-              #'schedule-cycle
-              [remaining-measures])))
+    (if-not (empty? remaining-measures)
+      (apply-at next-cycle-timestamp
+                #'schedule-cycle
+                [remaining-measures next-cycle-timestamp]))))
 
 (defn play-score [score]
-  (nome :bpm (:bpm score))
-  (schedule-cycle (:measures score)))
+  (binding [beats-per-minute (:bpm score)
+            beats-per-bar (:bpb score)]
+    (schedule-cycle (:measures score) (now))))
 
 (defn sin-blip [freq]
   {:instr sin-synth
    :freq freq
-   :amp 1
+   :amp 0.1
    :outbus 0})
 
 (def score
-  {:bpm 116
+  {:bpm 120
+   :bpb 3
    :measures [[1.0 [(sin-blip 440)]
                2.0 [(sin-blip 220)]
-               3.0 [(sin-blip 220)]
-               4.0 [(sin-blip 220)]]
+               3.0 [(sin-blip 220)]]
 
               [1.0 [(sin-blip 440)]
                2.0 [(sin-blip 220)]
-               3.0 [(sin-blip 220)]
-               4.0 [(sin-blip 220)]]
-              ]})
+               3.0 [(sin-blip 220)]]]})
 
 (play-score score)
--- test/ko/core_test.clj
+++ test/ko/core_test.clj
@@ -2,4 +2,10 @@
   (:use midje.sweet)
   (:require [ko.core :refer :all]))
 
-(facts "")
+(facts "about `quant-to-timestamp`"
+       (fact
+        (quant-to-timestamp 1.0 0 0.5) => 0.0)
+       (fact
+        (quant-to-timestamp 1.5 1000 0.5) => 1250.0)
+       (fact
+        (quant-to-timestamp 4.5 5000 0.5) => 6750.0))
