7bcac4d9ff07a5473557653c5fb5541e8559b2ce
bion <bion@citizencode.io>
2015-12-02 19:32:32 -0800

    Actually do it

--- src/ko/gesture.clj
+++ src/ko/gesture.clj
@@ -4,9 +4,25 @@
 
 (def ko-synth-templates (atom {}))
 
-(defmacro ko-defsynth [s-name args body]
+(defmacro var->keyword [var]
+  (keyword var))
+
+(defmacro var->string [var]
+  (str var))
+
+(defmacro ko-defsynth
+  "Define an overtone synth as per usual, but also store the
+  params andbody of the synth in `ko-synth-templates`"
+  [s-name args body]
   (let [kword-s-name (keyword s-name)]
-  `(do
-     (ot/defsynth ~s-name ~args ~body)
-     (swap! ko-synth-templates
-            #(assoc % ~kword-s-name '(~args ~body))))))
+    `(do
+       (ot/defsynth ~s-name ~args ~body)
+       (swap! ko-synth-templates
+              #(assoc % ~kword-s-name ['~args '~body])))))
+
+(defn with-mutations [synth mutations]
+  (let [[s-args s-body] ((var->keyword synth) ko-synth-templates)
+        s-name (str (var->string synth) "-" (gensym))
+        s-form (conj s-body s-args)
+        [s-name params ugen-form] (ot/synth-form s-name s-form)]
+    (prn s-name params ugen-form)))
--- test/ko/gesture_test.clj
+++ test/ko/gesture_test.clj
@@ -3,13 +3,25 @@
   (:use midje.sweet
         ko.gesture))
 
+(fact "var->keyword converts the var name to a keyword"
+      (var->keyword foo) => :foo
+      (var->keyword bar) => :bar
+      (var->keyword baz) => :baz)
+
+(fact "var->string converts the var name to a string"
+      (var->string foo) => "foo"
+      (var->string bar) => "bar"
+      (var->string baz) => "baz")
+
 (facts "about `ko-defsynth`"
        (against-background
-        [ (before :facts (reset! ko-synth-templates {})) ]
+        [(before :facts (reset! ko-synth-templates {}))
+         (after :facts (ns-unmap *ns* 'test-synth))]
+
         (fact "it adds the body of the synth to the ko-synth-templates atom"
-         (do
-           (ko-defsynth test-synth
-                        [one 1 two 2]
-                        (ot/out 0 0))
-           @ko-synth-templates) => {:test-synth '([one 1 two 2]
-                                                  (ot/out 0 0))})))
+              (do
+                (ko-defsynth test-synth
+                             [one 1 two 2]
+                             (ot/out 0 0))
+                @ko-synth-templates) => {:test-synth ['[one 1 two 2]
+                                                      '(ot/out 0 0)]})))
