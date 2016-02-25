ba37d2b40d6bdeb046bdc9145a81e4513025a3e5
Bion Johnson <bionjohnson@gmail.com>
2016-02-24 19:35:21 -0800

    Separate mutation handling from gestures

--- src/ko/gesture.clj
+++ src/ko/gesture.clj
@@ -1,20 +1,16 @@
 (ns ko.gesture
   [:require [overtone.core :as ot]]
+  [:use [ko.mutations]])
 
+(defonce synth-templates* (atom {}))
 (defonce living-gestures* (atom {}))
 (defonce groups* (atom {}))
-(defonce synth-templates* (atom {}))
 (defonce busses* (atom {}))
-
-(def keyword->symbol #(symbol (str (name %))))
 
 (defn remove-from-atom-map [atom-map k]
   (swap! atom-map
          (fn [atom-map-val]
            (apply dissoc (into [atom-map-val] k)))))
-
-(defn var->keyword [item]
-  (keyword (name item)))
 
 (defn add-bus [bus-name]
   (let [new-bus (ot/audio-bus)]
@@ -50,111 +46,6 @@
        (ot/defsynth ~s-name ~args ~body)
        (swap! synth-templates*
               #(assoc % ~kword-s-name '(~args ~body))))))
-
-(defn remove-param [param-list removed-param-names]
-  (let [param-map (apply hash-map param-list)
-        removed-params-symbols (map keyword->symbol removed-param-names)
-        remaining-params (clojure.set/difference (set (keys param-map))
-                                                 (set removed-params-symbols))]
-    (loop [params (transient [])
-           add-params remaining-params]
-      (cond (empty? add-params)
-            (persistent! params)
-
-            :else
-            (let [param-key (first add-params)
-                  param-val (param-key param-map)]
-              (conj! params param-key)
-              (conj! params param-val)
-              (recur params (rest add-params)))))))
-
-(defn enveloped-param
-  "creates an envelope record (not an overtone.core/envelope)
-  for a given mutating param"
-  [param initial snapshots]
-  (let [start-time (:timestamp initial)
-        levels (transient [(->> initial :spec param)])
-        durs (transient [])
-        curves (transient [])]
-
-    (loop [remaining-snapshots snapshots
-           last-timestamp start-time]
-
-      (if (empty? remaining-snapshots)
-        ;; return the envelope
-        (let [[levels durs curves] (map persistent! [levels durs curves])
-              levels (map resolve-synth-arg levels)]
-          {:param-name param
-           :envelope (ot/envelope levels durs curves)})
-
-        ;; keep recurring
-        (let [snapshot (first remaining-snapshots)
-              [level curve] (->> snapshot :spec param)
-              timestamp (:timestamp snapshot)
-              dur (- timestamp last-timestamp)]
-          (conj! levels level)
-          (conj! durs dur)
-          (conj! curves curve)
-          (recur (rest remaining-snapshots) timestamp))))))
-
-(defn- get-param-keys [g-events]
-  (remove #{:instr} (->> g-events first :spec keys)))
-
-(defn mutations->envelopes
-  "converts a vector of gesture states into a vector of hashes
-  of the form {:param-name \"some param\" :envelope envelope}}"
-  [gesture-events]
-  (let [param-keys (get-param-keys gesture-events)
-        initial (first gesture-events)
-        mutation-events (rest gesture-events)]
-    (for [param param-keys
-          :let [param-snapshots (filter #(param (% :spec)) mutation-events)]
-          :when (not (empty? param-snapshots))]
-      (enveloped-param param initial param-snapshots))))
-
-(defn envelope-binding-form
-  "given a collection of envelopes, returns a vector "
-  [envelopes]
-  (apply concat
-         (vec
-          (map #(vec
-                 [(keyword->symbol (:param-name %))
-                  (conj () (:envelope %) 'ot/env-gen)])
-               envelopes))))
-
-(defn apply-mutations
-  "for each mutating param, removes the param from the arglist and
-  injects an envelope in its place"
-  [s-template mutations]
-  (let [envelopes (mutations->envelopes mutations)
-        envelope-bindings (envelope-binding-form envelopes)
-        param-list (remove-param (first s-template) (map :param-name envelopes))
-        new-ugen-forms (conj () (last s-template) (vec envelope-bindings) 'let)
-        new-template (conj () new-ugen-forms param-list)]
-    new-template))
-
-(defn define-synth [s-name params ugen-form]
-  (eval `(ot/synth ~s-name ~params ~ugen-form)))
-
-(defn with-mutations
-  "Returns a function that plays the given synth with mutations applied"
-  [instr-name mutations]
-  (let [s-template (instr-name @synth-templates*)]
-    (if-not s-template
-      (throw (Exception. (str "no synth template found for: " instr-name))))
-
-    (let [s-name (symbol (str (name instr-name) "-" (gensym)))
-          s-template (apply-mutations s-template mutations)
-          [s-name params ugen-form] (ot/synth-form s-name s-template)]
-      (define-synth s-name params ugen-form))))
-
-(defn- resolve-optional-ssg-begin-args [args]
-  (let [arg-count (count args)]
-    (cond (= 2 arg-count)
-          [(second args) (first args)]
-
-          (= 1 arg-count)
-          [(first args) nil])))
 
 (defn- default-group []
   (ot/foundation-default-group))
@@ -210,15 +101,17 @@
      (println (str "registered group: " group-name))
      new-group)))
 
-(defn- resolve-position [position]
-  (let [[add-action group-name] (if (vector? position)
-                                  position
-                                  [:head position])
-        existing-group (@groups* group-name)]
-    (if (nil? existing-group)
-      (throw (Exception. (str "no group found with name "
-                              group-name))))
-    [add-action existing-group]))
+(defn resolve-position [position]
+  (if position
+    (default-group)
+    (let [[add-action group-name] (if (vector? position)
+                                    position
+                                    [:head position])
+          existing-group (@groups* group-name)]
+      (if (nil? existing-group)
+        (throw (Exception. (str "no group found with name "
+                                group-name))))
+      [add-action existing-group])))
 
 ;; returns a function that when called begins playing the gesture
 ;; and returns a representation of the playing gesture
@@ -235,7 +128,7 @@
           synth-args (conj synth-args position)
           synth-fn (if (empty? mutations)
                      instr
-                     (with-mutations instr-name mutations))]
+                     (with-mutations instr-name mutations synth-templates*))]
       #(apply synth-fn synth-args))))
 
 ;; clear previous definition of multimethod
@@ -255,20 +148,6 @@
   (fn [type & args]
     type))
 
-(defmethod begin :ssg
-  [g-type g-name spec & remaining]
-  (let [[mutations position] (resolve-optional-ssg-begin-args remaining)
-        position (if position (resolve-position position)
-                     (default-group))
-        g-instance (ssg-gest spec position mutations)]
-    #(do
-       (println (str "playing " g-name))
-       (if (g-name @living-gestures*)
-         (println "didn't begin" g-name ", key already found in living-gestures")
-         (let [g-nodes (g-instance)]
-           (swap! living-gestures*
-                  (fn [lgm] (assoc lgm g-name g-nodes))))))))
-
 (defn adjust
   "Send control messages to a running gesture.
   Messages are specified as alternating argument key value pairs"
@@ -287,3 +166,24 @@
      (doseq [node (map @living-gestures* g-names)]
        (ot/kill node))
      (remove-from-atom-map living-gestures* g-names)))
+
+(defn- resolve-optional-ssg-begin-args [args]
+  (let [arg-count (count args)]
+    (cond (= 2 arg-count)
+          [(second args) (first args)]
+
+          (= 1 arg-count)
+          [(first args) nil])))
+
+(defmethod begin :ssg
+  [g-type g-name spec & remaining]
+  (let [[mutations position] (resolve-optional-ssg-begin-args remaining)
+        position (resolve-position position)
+        g-instance (ssg-gest spec position mutations)]
+    #(do
+       (println (str "playing " g-name))
+       (if (g-name @living-gestures*)
+         (println "didn't begin" g-name ", key already found in living-gestures")
+         (let [g-nodes (g-instance)]
+           (swap! living-gestures*
+                  (fn [lgm] (assoc lgm g-name g-nodes))))))))
---
+++ src/ko/mutations.clj
@@ -0,0 +1,106 @@
+(ns ko.mutations
+  [:require [overtone.core :as ot]]
+  [:use [ko.gesture]])
+
+(def keyword->symbol #(symbol (str (name %))))
+
+(defn var->keyword [item]
+  (keyword (name item)))
+
+(defn define-synth [s-name params ugen-form]
+  (eval `(ot/synth ~s-name ~params ~ugen-form)))
+
+(defn remove-param [param-list removed-param-names]
+  (let [param-map (apply hash-map param-list)
+        removed-params-symbols (map keyword->symbol removed-param-names)
+        remaining-params (clojure.set/difference (set (keys param-map))
+                                                 (set removed-params-symbols))]
+    (loop [params (transient [])
+           add-params remaining-params]
+      (cond (empty? add-params)
+            (persistent! params)
+
+            :else
+            (let [param-key (first add-params)
+                  param-val (param-key param-map)]
+              (conj! params param-key)
+              (conj! params param-val)
+              (recur params (rest add-params)))))))
+
+(defn enveloped-param
+  "creates an envelope record (not an overtone.core/envelope)
+  for a given mutating param"
+  [param initial snapshots]
+  (let [start-time (:timestamp initial)
+        levels (transient [(->> initial :spec param)])
+        durs (transient [])
+        curves (transient [])]
+
+    (loop [remaining-snapshots snapshots
+           last-timestamp start-time]
+
+      (if (empty? remaining-snapshots)
+        ;; return the envelope
+        (let [[levels durs curves] (map persistent! [levels durs curves])
+              levels (map resolve-synth-arg levels)]
+          {:param-name param
+           :envelope (ot/envelope levels durs curves)})
+
+        ;; keep recurring
+        (let [snapshot (first remaining-snapshots)
+              [level curve] (->> snapshot :spec param)
+              timestamp (:timestamp snapshot)
+              dur (- timestamp last-timestamp)]
+          (conj! levels level)
+          (conj! durs dur)
+          (conj! curves curve)
+          (recur (rest remaining-snapshots) timestamp))))))
+
+(defn- get-param-keys [g-events]
+  (remove #{:instr} (->> g-events first :spec keys)))
+
+(defn mutations->envelopes
+  "converts a vector of gesture states into a vector of hashes
+  of the form {:param-name \"some param\" :envelope envelope}}"
+  [gesture-events]
+  (let [param-keys (get-param-keys gesture-events)
+        initial (first gesture-events)
+        mutation-events (rest gesture-events)]
+    (for [param param-keys
+          :let [param-snapshots (filter #(param (% :spec)) mutation-events)]
+          :when (not (empty? param-snapshots))]
+      (enveloped-param param initial param-snapshots))))
+
+(defn envelope-binding-form
+  "given a collection of envelopes, returns a vector "
+  [envelopes]
+  (apply concat
+         (vec
+          (map #(vec
+                 [(keyword->symbol (:param-name %))
+                  (conj () (:envelope %) 'ot/env-gen)])
+               envelopes))))
+
+(defn apply-mutations
+  "for each mutating param, removes the param from the arglist and
+  injects an envelope in its place"
+  [s-template mutations]
+  (let [envelopes (mutations->envelopes mutations)
+        envelope-bindings (envelope-binding-form envelopes)
+        param-list (remove-param (first s-template) (map :param-name envelopes))
+        new-ugen-forms (conj () (last s-template) (vec envelope-bindings) 'let)
+        new-template (conj () new-ugen-forms param-list)]
+    new-template))
+
+
+(defn with-mutations
+  "Returns a function that plays the given synth with mutations applied"
+  [instr-name mutations templates*]
+  (let [s-template (instr-name @templates*)]
+    (if-not s-template
+      (throw (Exception. (str "no synth template found for: " instr-name))))
+
+    (let [s-name (symbol (str (name instr-name) "-" (gensym)))
+          s-template (apply-mutations s-template mutations)
+          [s-name params ugen-form] (ot/synth-form s-name s-template)]
+      (define-synth s-name params ugen-form))))
