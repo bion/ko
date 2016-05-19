ca59ceea90aa428bdcbc69072794be6e93cf767e
Bion Johnson <bionjohnson@gmail.com>
2016-05-18 22:09:01 -0700

    Add schema, specter

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[com.rpl/specter "0.9.3"]
                  [org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]
-                 [prismatic/schema "1.1.1"]]
+                 [prismatic/schema "1.1.1"]
+                 [com.rpl/specter "0.10.0"]]
   :plugins [[lein-checkouts "1.1.0"]]
   :profiles {:uberjar {:aot :all}})
