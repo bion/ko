8c43f37ee86d6819663d72e476faa498532e1caa
Bion Johnson <bionjohnson@gmail.com>
2016-08-11 22:58:12 -0700

    Upgrade specter

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
                  [com.rpl/specter "0.9.3"]
                  [overtone "0.10.1"]
                  [prismatic/schema "1.1.1"]
-                 [com.rpl/specter "0.10.0"]]
+                 [com.rpl/specter "0.12.0"]]
   :plugins [[lein-checkouts "1.1.0"]]
   :profiles {:uberjar {:aot :all}})
