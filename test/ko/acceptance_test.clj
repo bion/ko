4e4e7000793a5efae2c068b579cf947828d5db17
Bion Johnson <bionjohnson@gmail.com>
2016-08-11 22:58:29 -0700

    Update acceptance test

--- test/ko/acceptance_test.clj
+++ test/ko/acceptance_test.clj
@@ -1,38 +1,44 @@
 (ns ko.acceptance-test
-  (:require [overtone.core :as ot] :reload)
   (:use [ko.gesture]
         [ko.scheduling]
+        [overtone.core]
         [ko.score] :reload))
 
-(if (ot/server-connected?)
-  (do (ot/stop)
+(if (server-connected?)
+  (do (stop)
       (reset-groups!))
-  (ot/boot-server))
+  (boot-server))
+
+(ko-defsynth out-synth
+             [in-bus 10]
+             (out:ar 0 (in:ar in-bus)))
 
 (ko-defsynth test-synth
              [freq 1 amp 1 bus 0]
-             (ot/out bus (* (ot/saw freq) amp)))
+             (out bus (* (saw freq) amp)))
 
 (ko-defsynth test-filter
              [in-bus 8 out-bus 0 cutoff 440]
-             (let [sig (ot/in in-bus)]
-               (ot/out out-bus (ot/lpf sig cutoff))))
+             (let [sig (in in-bus)]
+               (out out-bus (lpf sig cutoff))))
 
-(def source-spec {:instr test-synth
-                  :freq 220
-                  :amp -12
-                  :bus "test-bus"})
+(def source-spec
+  {:instr test-synth
+   :freq 220
+   :amp -20
+   :bus "test-bus"})
 
-(def filt-spec {:instr test-filter
-                :in-bus "test-bus"
-                :out-bus 0
-                :cutoff 1000})
+(def filt-spec
+  {:instr test-filter
+   :in-bus "test-bus"
+   :out-bus "out-bus"
+   :cutoff 1000})
 
 (register-group "source")
 (register-group "filter" "source" :after)
 
 (defn note->hz [note-name]
-  (ot/midi->hz (ot/note note-name)))
+  (midi->hz (note note-name)))
 
 (defn notes [& notes]
   (map note->hz notes))
@@ -42,7 +48,8 @@
   (beats-per-minute 108)
 
   (label :beginning)
-  1 [(begin :ssg :filt filt-spec "filter")
+  1 [(begin :ssg :out {:instr out-synth :in-bus "out-bus"})
+     (begin :ssg :filt filt-spec "filter")
      (begin :msg :one (assoc source-spec
                              :freq (notes :F3 :Gb3 :Bb3 :F4 :Bb4 :F5))
             "source")]
@@ -60,38 +67,35 @@
 
   (silent-measure)
 
-  1 [(finish :two)])
-
-(meta test-score)
-(count test-score)
+  1 [(finish :two :out)])
 
 (play-score test-score)
 (stop-score test-score)
-(ot/pp-node-tree)
+(pp-node-tree)
 
 (clojure.pprint/pprint test-score)
-(clojure.pprint/pprint (ot/midi-connected-devices))
+(clojure.pprint/pprint (midi-connected-devices))
 
-(ot/event-debug-on)
-(ot/event-debug-off)
+(event-debug-on)
+(event-debug-off)
 
 (defonce kbd-notes* (atom {}))
 
-(ot/on-event [:midi :note-on]
-             (fn [e] (let [note (:note e)
-                           vel  (:velocity e)
-                           synth (test-synth :freq (ot/midi->hz note)
-                                             :amp (* 0.2 (/ vel 128)))]
-                       (swap! kbd-notes* #(assoc % note synth))))
-             ::kbd-note-on-handler)
+(on-event [:midi :note-on]
+          (fn [e] (let [note (:note e)
+                        vel  (:velocity e)
+                        synth (test-synth :freq (midi->hz note)
+                                          :amp (* 0.2 (/ vel 128)))]
+                    (swap! kbd-notes* #(assoc % note synth))))
+          ::kbd-note-on-handler)
 
-(ot/on-event [:midi :note-off]
-             (fn [e]
-               (let [note (:note e)
-                     synth (@kbd-notes* note)]
-                 (ot/kill synth)
-                 (swap! kbd-notes* #(assoc note nil))))
-             ::kbd-note-off-handler)
+(on-event [:midi :note-off]
+          (fn [e]
+            (let [note (:note e)
+                  synth (@kbd-notes* note)]
+              (kill synth)
+              (swap! kbd-notes* #(assoc note nil))))
+          ::kbd-note-off-handler)
 
-(ot/remove-event-handler ::kbd-note-on-handler)
-(ot/remove-event-handler ::kbd-note-off-handler)
+(remove-event-handler ::kbd-note-on-handler)
+(remove-event-handler ::kbd-note-off-handler)
