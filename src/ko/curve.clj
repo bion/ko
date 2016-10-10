74841e8534c500b80d61dba9837b17d12d9b74d3
Bion Johnson <bionjohnson@gmail.com>
2016-10-10 15:45:24 -0700

    Clear error for incorrect curve params

--- src/ko/curve.clj
+++ src/ko/curve.clj
@@ -49,6 +49,12 @@
 
         ;; keep recurring
         (let [snapshot (first remaining-snapshots)
+              _ (if-not (coll? (->> snapshot :spec param))
+                  (throw (Exception.
+                          (str "synth params for `curve` actions be a tuple "
+                               "containing the new value and a curve. "
+                               "instead param " param " is: "
+                               (->> snapshot :spec param)))))
               [level curve] (->> snapshot :spec param)
               timestamp (:timestamp snapshot)
               dur (- timestamp last-timestamp)]
